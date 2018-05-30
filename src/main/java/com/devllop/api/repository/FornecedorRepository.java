cf18ee4f2381f070397b525b2fca763723b6f695
deividbenetti <deivid_bn_@hotmail.com>
2018-05-29 22:15:44 -0300

    commit Fornecedor

---
+++ src/main/java/com/devllop/api/repository/FornecedorRepository.java
@@ -0,0 +1,9 @@
+package com.devllop.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.devllop.api.model.Fornecedor;
+
+public interface FornecedorRepository  extends JpaRepository<Fornecedor, Long>{
+	
+
+}
---
+++ src/main/java/com/devllop/api/service/FornecedorService.java
@@ -0,0 +1,24 @@
+package com.devllop.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import com.devllop.api.model.Fornecedor;
+import com.devllop.api.repository.FornecedorRepository;
+
+@Service
+public class FornecedorService {
+	
+	@Autowired
+	private FornecedorRepository fornecedorRepository;
+	
+	public Fornecedor atualizar(Long id, Fornecedor fornecedor) {
+		Fornecedor fornecedorSalvo = fornecedorRepository.findOne(id);
+		if (fornecedorSalvo == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(fornecedor, fornecedorSalvo, "id");
+		return fornecedorRepository.save(fornecedorSalvo);
+	}
+}
